5e19c059a17163c9fbbda112381d0414559deee8
RemcoBeumer <568957@edu.rocmn.nl>
2021-12-15 11:22:12 +0100

    head working

--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<head>
-<title>CSS Template</title>
-<link rel="stylesheet" href="./media/style.css">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-</head>
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include 'header.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<header>
+<title>CSS Template</title>
+<link rel='stylesheet' href="./media/style.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</header>
+
