ca539b2fa988defa824f6b27141ddaf2dab789ef
Job <566351@edu.rocmn.nl>
2022-01-17 12:33:02 +0100

    dashboard toegevoegd

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<?php
+// Het bestand auth_session.php opnemen op alle pagina's van het gebruikerspaneel
+include("auth_session.php");
+
+require('db.php');
+include("auth_session.php");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard - klantenbeheer</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="form">
+        <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
+        <p>Je bent nu in het klantenbeheer</p>
+        <p><a href="logout.php">Uitloggen</a></p>
+    </div>
+</body>
+</html>
