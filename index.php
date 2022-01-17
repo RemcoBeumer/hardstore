7a2fe6f254af039546b0c4b10b3f301fedcfea42
Job <566351@edu.rocmn.nl>
2022-01-17 20:06:37 +0100

    navbar aangepast, producten toegevoegd

--- index.php
+++ index.php
@@ -7,23 +7,59 @@
     <link rel="stylesheet" href="./media/index.css">
   </head>
 
-  <body>
-    <div class="topnav" id="topnav">
-      <a href="#inloggen">Inloggen</a>
-      <a href="#contact">Contact</a>
-      <a href="#winkel">Producten</a>
-      <a href="#home" class="active">Home</a>
-    </div>
-
-<div class="card">
-  <h2 style="text-align:center">Laptops</h2>
-  <img src="./media/laptop.png" alt="laptop" style="width:100%">
-  <p class="price">vanaf €399.99</p>
-  <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
-  <p><button>Bekijk producten</button></p>
-
-  
+  <div class="navbar">
+    <div class="heading">
+      <h4>Hardstore</h4>
 </div>
+  <a href="login.pgp">Inloggen</a>
+  <a href="contact.php">Contact</a>
+  <div class="dropdown">
+    <button class="dropbtn">Producten
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="laptops.php">Laptops</a>
+      <a href="televisies.php">Televisies</a>
+      <a href="telefoons.php">Telefoons</a>
+    </div>
+  </div> 
+  <a href="home.php">Home</a>
+</div>
+
+<div class="row">
+
+  <div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Bekijk producten</button></p>
+</div>
+</div>
+
+  	<div class="column">
+      <div class="card">
+        <h2 style="text-align:center">Televisies</h2>
+        <p class="price">Vanaf €289.99</p>
+        <p>Some text</p>
+        <p>Some text</p>
+        <p><button>Bekijk producten</button>
+</div>
+</div>
+  
+    <div class="column">
+      <div class="card">
+        <h2 style="text-align:center">Telefoons</h2>
+        <p class="price">Vanaf €199.99</p>
+        <p>Some text</p>
+        <p>Some text</p>
+        <p><button>Bekijk producten</button>
+</div>
+</div>
+
+</div>
+
 
   </body>
 </html>
