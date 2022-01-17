10dd21b9a3feef14fc2b24a77467eda454ef330d
Job <566351@edu.rocmn.nl>
2022-01-17 21:21:31 +0100

    laptops pagina toegevoegd

---
+++ producten/laptops.php
@@ -0,0 +1,127 @@
+
+<html lang="eng">
+  <head>
+    <title>Hardstore</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../media/index.css">
+  </head>
+
+  <div class="navbar">
+    <div class="heading">
+      <h4>Hardstore</h4>
+</div>
+  <a href="../login.php">Inloggen</a>
+  <a href="../contact.php">Contact</a>
+  <div class="dropdown">
+    <button class="dropbtn">Producten
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="laptops.php">Laptops</a>
+      <a href="televisies.php">Televisies</a>
+      <a href="telefoons.php">Telefoons</a>
+    </div>
+  </div> 
+  <a href="../index.php">Home</a>
+</div>
+
+<!-- Producten area -->
+
+<div class="row">
+
+  <div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+  
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+</div>
+
+
+  </body>
+</html>
