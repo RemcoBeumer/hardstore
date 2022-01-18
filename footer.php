3b76d24ced5fab15e2240607fa748be78f323883
Job <566351@edu.rocmn.nl>
2022-01-18 10:53:02 +0100

    include system

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="footer"> 
+<p>Copyright © 2022 Hardstore. All rights are reserved.</p>
+</div>
---
+++ navbar.php
@@ -0,0 +1,21 @@
+<div class="navbar">
+    <div class="heading">
+      <h4>Hardstore</h4>
+</div>
+<div class="login">
+  <a href="login.php">Inloggen</a>
+  <a href="registration.php">Registreren</a>
+</div>
+  <a href="index.php">Home</a>
+  <div class="dropdown">
+    <button class="dropbtn">Producten
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="laptops.php">Laptops</a>
+      <a href="televisies.php">Televisies</a>
+      <a href="telefoons.php">Telefoons</a>
+    </div>
+  </div> 
+  <a href="contact.php">Contact</a>
+</div>
