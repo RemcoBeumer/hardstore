61f7e67e0080b20981d54d81d248bd2eaf0ac11d
Job <566351@edu.rocmn.nl>
2022-01-18 10:52:02 +0100

    telefoons page include system toegevoegd

---
+++ telefoons.php
@@ -0,0 +1,63 @@
+<?php include 'header.php' ?>
+
+<!-- Producten area -->
+<div class="row">
+  <div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+  
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">Laptops</h2>
+    <p class="price">vanaf €399.99</p>
+      <p>Some text</p>
+      <p>Some text</p>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+
+</div>
+
+<?php include 'footer.php' ?>
