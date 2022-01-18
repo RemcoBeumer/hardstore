720ae464bf849249497a461d06e6e7d0bcbb04ab
Job <566351@edu.rocmn.nl>
2022-01-18 10:53:27 +0100

    verwerken.php page include system toegevoegd

--- verwerken.php
+++ verwerken.php
@@ -1,31 +1,5 @@
 
-
-<html lang="eng">
-  <head>
-    <title>Hardstore</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./media/index.css">
-  </head>
-
-  <div class="navbar">
-    <div class="heading">
-      <h4>Hardstore</h4>
-</div>
-  <a href="login.php">Inloggen</a>
-  <a href="contact.php">Contact</a>
-  <div class="dropdown">
-    <button class="dropbtn">Producten
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="laptops.php">Laptops</a>
-      <a href="televisies.php">Televisies</a>
-      <a href="telefoons.php">Telefoons</a>
-    </div>
-  </div> 
-  <a href="index.php">Home</a>
-</div>
+<?php include 'header.php' ?>
 
 <h1>Bedankt voor het contact opnemen!</h1>
 	
@@ -41,5 +15,4 @@
 
 	?>
 
-  </body>
-</html>
+<?php include 'footer.php' ?>
