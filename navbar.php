cff86bc7b6c4e682bc1b040867f280f0afaeeae3
Job <566351@edu.rocmn.nl>
2022-01-24 09:43:47 +0100

    styles veranderd

--- navbar.php
+++ navbar.php
@@ -1,9 +1,15 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./media/index.css">
+  </head>
+
 <div class="navbar">
     <div class="heading">
       <h4>Hardstore</h4>
 </div>
 <div class="login">
-  <a href="login.php">Inloggen</a>
+<a href="login.php">Inloggen</a>
   <a href="registration.php">Registreren</a>
 </div>
   <a href="index.php">Home</a>
