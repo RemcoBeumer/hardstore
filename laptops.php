f51f322a7bf78932fbafd13c58589f1bc61a6eea
RemcoBeumer <568957@edu.rocmn.nl>
2022-01-24 12:52:41 +0100

    laptops gedeelte

--- laptops.php
+++ laptops.php
@@ -4,29 +4,36 @@
 <div class="row">
   <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Laptops</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    <h2 style="text-align:center">Lenovo IdeaPad 3 14IIL05-81WD00BLMH</h2>
+    
+    <p class="price">Prijs: €429.99</p>
+     <img src= "./media/lenovoideapad.png" style='width: 100%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
 <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Laptops</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    <h2 style="text-align:center">MSI Gaming GF76 11UC-015NL Gaming Laptop - 144hz</h2>
+
+    <p class="price">Prijs: €1.249,00</p>
+    <img src= "./media/MSIGamingGF76.png" style='width: 100%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
   
 <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Laptops</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    <h2 style="text-align:center">ASUS ROG Zephyrus G14 GA401QC-K2167T  Gaming Laptop  </h2>
+    <p class="price">Prijs: €1.499,00</p>
+    <img src= "./media/ASUSROG.png" style='width: 100%'>
+      <p><button>Voeg toe aan winkelmandje</button></p>
+</div>
+</div>
+<div class="column">
+    <div class="card">
+    <h2 style="text-align:center">HP Chromebook 14b-nb0350nd</h2>
+    <p class="price">Prijs: €799</p>
+    <img src= "./media/chromebook.png" style='width: 100%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
@@ -34,17 +41,7 @@
     <div class="card">
     <h2 style="text-align:center">Laptops</h2>
     <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
-      <p><button>Voeg toe aan winkelmandje</button></p>
-</div>
-</div>
-<div class="column">
-    <div class="card">
-    <h2 style="text-align:center">Laptops</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
