850cc4aa31b407cbad218fd09e0549ba4469ae31
Job <566351@edu.rocmn.nl>
2022-01-26 11:18:39 +0100

    homepage werkend gemaakt

--- index.php
+++ index.php
@@ -3,34 +3,40 @@
 
 <div class="row">
 
+<form action="laptops.php">
   <div class="column">
     <div class="card">
     <h2 style="text-align:center">Laptops</h2>
     <p class="price">vanaf €399.99</p>
       <p>Some text</p>
       <p>Some text</p>
-      <p><button formaction="./producten/laptops.php">Bekijk producten</button></p>
+      <p><button type="submit">Bekijk producten</button></p>
 </div>
+</form>
 </div>
 
+  <form action="televisies.php">
   	<div class="column">
       <div class="card">
         <h2 style="text-align:center">Televisies</h2>
         <p class="price">Vanaf €289.99</p>
         <p>Some text</p>
         <p>Some text</p>
-        <p><button formaction="./producten/televisies.php">Bekijk producten</button>
+        <p><button type="submit">Bekijk producten</button>
+</div>
 </div>
 </div>
   
+  <form action="telefoons.php">
     <div class="column">
       <div class="card">
         <h2 style="text-align:center">Telefoons</h2>
         <p class="price">Vanaf €199.99</p>
         <p>Some text</p>
         <p>Some text</p>
-        <p><button formaction="./producten/telefoons.php">Bekijk producten</button>
+        <p><button type="submit">Bekijk producten</button>
 </div>
+</form>
 </div>
 
 </div>
