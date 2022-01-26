6f996820bbaf2e946b9556b2d143d3cb15df1d22
Your Name <you@example.com>
2022-01-26 10:35:39 +0100

    added some more phones

--- telefoons.php
+++ telefoons.php
@@ -21,7 +21,7 @@
   
 <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Telfoon</h2>
+    <h2 style="text-align:center">Xiaomi Redmi 9C 128GB Zwart</h2>
     <p class="price">Prijs: €149.46</p>
     <img src= "./media/XIAOMI.png" style='width: 68%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
@@ -29,28 +29,25 @@
 </div>
 <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Telfoon</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    <h2 style="text-align:center">Samsung Galaxy A52s 5G 128GB A528 Zwart</h2>
+    <p class="price">vanaf €330.00</p>
+    <img src= "./media/telefoon1.png" style='width: 68%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
 <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Telfoon</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    <h2 style="text-align:center">Poco X3 Pro 256GB Zwart</h2>
+    <p class="price">vanaf €315.00</p>
+    <img src= "./media/telefoon2.png" style='width: 75%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
 <div class="column">
     <div class="card">
-    <h2 style="text-align:center">Telefoon</h2>
-    <p class="price">vanaf €399.99</p>
-      <p>Some text</p>
-      <p>Some text</p>
+    <h2 style="text-align:center">Samsung Galaxy S21 FE 5G 128GB G990 Zwart</h2>
+    <p class="price">vanaf €620.00</p>
+    <img src= "./media/telefoon3.png" style='width: 68%'>
       <p><button>Voeg toe aan winkelmandje</button></p>
 </div>
 </div>
